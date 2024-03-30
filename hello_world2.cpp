f2907ad3431178c5f61b6f65b4f65ccbb9cac498
Your Name <you@example.com>
2024-03-30 20:40:07 +0000

    first commit

--- hello_world2.cpp
+++ hello_world2.cpp
@@ -1,11 +1,20 @@
 #include <fstream>
 #include <iostream>
 
+int fib(int x);
+
 int main(int argc, char *argv[])
 {
-	std::ofstream ofs ("hello.txt", std::ofstream::out);
-	ofs << "Hello CS203!\n";
-	ofs.close();
-	std::cout << "Execution Complete" << std::endl;
-	return 0;  
+    int x;
+    std::ofstream ofs ("hello.txt", std::ofstream::out);
+    ofs << "Hello CS203! My e-mail address is \n";
+    ofs << "The "<< x << "th fibonacci number is "<< fib(x) <<"\n";
+    ofs.close();
+    std::cout << "Execution Complete" << std::endl;
+    return 0;  
+}
+
+int fib(int x)
+{
+    return 0;
 }
